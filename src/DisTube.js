59370190a872b6a54090717e54c49884ef9e93a6
Skick <skick1234@gmail.com>
2020-10-27 18:41:51 +0700

    Add 5 more filters

--- src/DisTube.js
+++ src/DisTube.js
@@ -50,13 +50,18 @@ const DisTubeOptions = {
  * @prop {string} 3d `@2.0.0`
  * @prop {string} bassboost `@2.0.0`
  * @prop {string} echo `@2.0.0` 
+ * @prop {string} karaoke `@2.0.0` 
+ * @prop {string} nightcore `@2.0.0`
+ * @prop {string} vaporwave `@2.0.0`
  * @prop {string} flanger `@2.4.0`
  * @prop {string} gate `@2.4.0`
  * @prop {string} haas `@2.4.0`
- * @prop {string} karaoke `@2.0.0` 
- * @prop {string} nightcore `@2.0.0`
  * @prop {string} reverse `@2.4.0`
- * @prop {string} vaporwave `@2.0.0`
+ * @prop {string} surround `@2.7.0`
+ * @prop {string} mcompand `@2.7.0`
+ * @prop {string} phaser `@2.7.0`
+ * @prop {string} tremolo `@2.7.0`
+ * @prop {string} earwax `@2.7.0`
  */
 const ffmpegFilters = {
   "3d": "apulsator=hz=0.125",
@@ -69,6 +74,11 @@ const ffmpegFilters = {
   nightcore: "asetrate=48000*1.25,aresample=48000,equalizer=f=40:width_type=h:width=50:g=10",
   reverse: 'areverse',
   vaporwave: "asetrate=48000*0.8,aresample=48000,atempo=1.1",
+  mcompand: 'mcompand',
+  phaser: 'aphaser',
+  tremolo: 'tremolo',
+  surround: "surround",
+  earwax: "earwax",
 }
 
 /**
