daddfbcec062164bad6cbaef61f06ecbd45d5edd
Skick <skick1234@gmail.com>
2020-11-02 00:57:12 +0700

    Split SearchResult to another file

--- typings/Song.d.ts
+++ typings/Song.d.ts
@@ -1,8 +1,6 @@
-/// <reference types="@distube/ytsr" />
 export = Song;
 import Discord from "discord.js";
 import ytdl from "ytdl-core";
-import ytsr from "ytsr";
 /** Class representing a song. */
 declare class Song {
     /**
@@ -93,66 +91,6 @@ declare class Song {
      * @type {number}
      */
     reposts: number;
-    /**
-     * @deprecated use `Song.name` instead
-     * @type {string}
-     */
-    title: string;
-    /**
-     * @deprecated use `Song.url` instead
-     * @type {string}
-     */
-    link: string;
-}
-declare namespace Song {
-    export { SearchResult };
-}
-declare class SearchResult {
-    /**
-     * Create a search result.
-     * @param {ytsr.Video} info Video info
-     */
-    constructor(info: ytsr.Video);
-    /**
-     * Youtube video id
-     * @type {string}
-     */
-    id: string;
-    /**
-     * Song name aka video title.
-     * @type {string}
-     */
-    name: string;
-    /**
-     * Song duration.
-     * @type {number}
-     */
-    duration: number;
-    /**
-     * Formatted duration string `hh:mm:ss` or `mm:ss`.
-     * @type {string}
-     */
-    formattedDuration: string;
-    /**
-     * Song URL.
-     * @type {string}
-     */
-    url: string;
-    /**
-     * Song thumbnail.
-     * @type {string}
-     */
-    thumbnail: string;
-    /**
-     * Indicates if the video is an active live.
-     * @type {boolean}
-     */
-    isLive: boolean;
-    /**
-     * Song views count
-     * @type {number}
-     */
-    views: number;
     /**
      * @deprecated use `Song.name` instead
      * @type {string}
