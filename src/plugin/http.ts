e9769fa841447dae9f32530d39cbfe5d1798da40
Skick <skick1234@gmail.com>
2021-08-08 10:35:37 +0700

    refactor(Plugins): clean code

--- src/plugin/http.ts
+++ src/plugin/http.ts
@@ -1,40 +1,15 @@
 import http from "http";
 import ExtractorPlugin from "../struct/ExtractorPlugin";
-import { Song } from "..";
-import { URL } from "url";
 import { GuildMember } from "discord.js";
-
-const getResponseHeaders = (url: string | URL | http.RequestOptions) =>
-  new Promise((resolve, reject) => {
-    http
-      .get(url)
-      .on("response", res => resolve(res.headers))
-      .on("error", reject);
-  });
+import { resolveHttpSong, validateAudioURL } from "..";
 
 export class HTTPPlugin extends ExtractorPlugin {
   async validate(url: string) {
-    if (new URL(url).protocol.toLowerCase() !== "http:") {
-      return false;
-    }
-    const headers = (await getResponseHeaders(url)) as any;
-    const type = headers["content-type"];
-    if (type.startsWith("audio")) {
-      return true;
-    }
-    return false;
+    return validateAudioURL(http, "http:", url);
   }
-  // eslint-disable-next-line require-await
+
   async resolve(url: string, member: GuildMember) {
-    url = url.replace(/\/+$/, "");
-    return new Song(
-      {
-        name: url.substring(url.lastIndexOf("/") + 1).replace(/((\?|#).*)?$/, "") || url,
-        url,
-      },
-      member,
-      "http",
-    );
+    return resolveHttpSong("http", url, member);
   }
 }
 
--- src/plugin/https.ts
+++ src/plugin/https.ts
@@ -1,40 +1,55 @@
+import http from "http";
 import https from "https";
 import ExtractorPlugin from "../struct/ExtractorPlugin";
-import { Song } from "..";
 import { URL } from "url";
+import { Song } from "..";
 import { GuildMember } from "discord.js";
 
-const getResponseHeaders = (url: string | https.RequestOptions | URL) =>
+export const getResponseHeaders = async (
+  httpModule: typeof http | typeof https,
+  url: string,
+): Promise<http.IncomingHttpHeaders> =>
   new Promise((resolve, reject) => {
-    https
+    httpModule
       .get(url)
-      .on("response", res => resolve(res.headers))
+      .on("response", res => {
+        resolve(res.headers);
+      })
       .on("error", reject);
   });
 
-export class HTTPSPlugin extends ExtractorPlugin {
-  async validate(url: string) {
-    if (new URL(url).protocol.toLowerCase() !== "https:") {
-      return false;
-    }
-    const headers = (await getResponseHeaders(url)) as any;
-    const type = headers["content-type"];
-    if (type.startsWith("audio")) {
-      return true;
-    }
+export const validateAudioURL = async (httpModule: typeof http | typeof https, protocol: string, url: string) => {
+  if (new URL(url).protocol.toLowerCase() !== protocol) {
     return false;
   }
-  // eslint-disable-next-line require-await
+  const headers = await getResponseHeaders(httpModule, url),
+    type = headers["content-type"];
+  if (type?.startsWith("audio")) {
+    return true;
+  }
+  return false;
+};
+
+// eslint-disable-next-line @typescript-eslint/require-await
+export const resolveHttpSong = async (source: string, url: string, member: GuildMember) => {
+  url = url.replace(/\/+$/, "");
+  return new Song(
+    {
+      name: url.substring(url.lastIndexOf("/") + 1).replace(/((\?|#).*)?$/, "") || url,
+      url,
+    },
+    member,
+    source,
+  );
+};
+
+export class HTTPSPlugin extends ExtractorPlugin {
+  async validate(url: string) {
+    return validateAudioURL(https, "https:", url);
+  }
+
   async resolve(url: string, member: GuildMember) {
-    url = url.replace(/\/+$/, "");
-    return new Song(
-      {
-        name: url.substring(url.lastIndexOf("/") + 1).replace(/((\?|#).*)?$/, "") || url,
-        url,
-      },
-      member,
-      "https",
-    );
+    return resolveHttpSong("https", url, member);
   }
 }
 
