5c7ea9e10e31a34b8a94459ee8be6c03ed58675a
Skick <skick1234@gmail.com>
2021-12-14 09:27:07 +0700

    test: update tests

--- src/core/__test__/DisTubeOptions.test.ts
+++ src/core/__test__/DisTubeOptions.test.ts
@@ -27,23 +27,23 @@ test("Validate DisTubeOptions", () => {
   const n: any = NaN;
   expect(() => {
     new Options(n);
-  }).toThrow("Expected 'object' for 'DisTubeOptions', but got NaN");
+  }).toThrow("Expected 'object' for 'DisTubeOptions', but got null (Number)");
   for (const key of Object.keys(defaultOptions).filter(o => o !== "plugins")) {
     const options = {};
     options[key] = n;
     expect(() => {
       new Options(options);
-    }).toThrow(`Expected '${typeof defaultOptions[key]}' for 'DisTubeOptions.${key}', but got NaN`);
+    }).toThrow(`Expected '${typeof defaultOptions[key]}' for 'DisTubeOptions.${key}', but got null (Number)`);
   }
   expect(() => {
     new Options({ plugins: "undefined" as any });
-  }).toThrow("Expected 'Array<Plugin>' for 'DisTubeOptions.plugins', but got 'undefined'");
+  }).toThrow("Expected 'Array<Plugin>' for 'DisTubeOptions.plugins', but got \"undefined\" (String)");
   expect(() => {
     new Options({ youtubeCookie: 1 as any });
-  }).toThrow("Expected 'string' for 'DisTubeOptions.youtubeCookie', but got 1");
+  }).toThrow("Expected 'string' for 'DisTubeOptions.youtubeCookie', but got 1 (Number)");
   expect(() => {
     new Options({ youtubeIdentityToken: {} as any });
-  }).toThrow("Expected 'string' for 'DisTubeOptions.youtubeIdentityToken', but got Object");
+  }).toThrow("Expected 'string' for 'DisTubeOptions.youtubeIdentityToken', but got {} (Object)");
   expect(() => {
     new Options({ invalidKey: "an invalid key" } as any);
   }).toThrow("'invalidKey' does not need to be provided in DisTubeOptions");
